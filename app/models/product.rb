015016bfe3df05c5447426b5fba1520dd166486c
MargaretDobson <margaretdobson50@gmail.com>
2018-10-31 15:03:45 +1000

    .

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :user
-<<<<<<< HEAD
-    has_attached_file :image
-=======
     has_one_attached :image
 
     def can_change?(user)
         self.user == user || user.has_role?(:admin)
->>>>>>> 3ccceeccbe0f4f333f956d1ba1cd5c7b1ae7b873
-end
+    end
 
+end
