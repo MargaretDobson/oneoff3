e5317be89009f63a779d105d146d1a0f890f9711
Matt <M.martin747@gmail.com>
2018-11-02 09:04:30 +1000

    breaking stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  rolify
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,5 +13,13 @@ class User < ApplicationRecord
 
   has_many :product
   has_one :address
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
   
 end
