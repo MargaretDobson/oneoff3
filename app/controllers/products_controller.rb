914f1f72659c23433fbc5f84b8f96a952969e884
Danylo Denysov <danyldezwaan@hotmail.com>
2018-11-05 09:46:26 +1000

    Additional changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+
 class ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
@@ -89,4 +90,3 @@ class ProductsController < ApplicationController
     end
 
 end
-
