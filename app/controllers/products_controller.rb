78b49296cd7826eba43eceb01523ebfc0497e79e
MargaretDobson <margaretdobson50@gmail.com>
2018-11-05 12:58:45 +1000

    .

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,6 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
-    p "----------Project inspect:" + @product.inspect
   end
 
   # GET /products/1/edit
