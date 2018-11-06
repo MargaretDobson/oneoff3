167602e0ed496e0c8098540e473bcffb83633277
Danylo Denysov <danyldezwaan@hotmail.com>
2018-11-06 16:27:33 +1000

    Stripe configs

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 
 #Initializer for stripe
 Rails.configuration.stripe = {
-    :publishable_key => '',
-    :secret_key => ''
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
