867a36ae7faf71513a91c4ab813d4c3809494beb
MargaretDobson <margaretdobson50@gmail.com>
2018-11-09 11:28:27 +1000

    hopefully resolved

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'switchery-rails'
 
 
 gem "dotenv-rails", "~> 2.5", :groups => [:development, :test] 
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,13 @@
 
 #Initializer for stripe
 Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
+<<<<<<< HEAD
+    :publishable_key => 'pk_test_R981wojulmBDDvE4tFB09Iql',
+    :secret_key => 'sk_test_e3VtxkkZUi8NSv38psDYa12C'
+=======
+    :publishable_key => pk_test_R981wojulmBDDvE4tFB09Iql,
+    :secret_key => sk_test_e3VtxkkZUi8NSv38psDYa12C
+>>>>>>> parent of d39d9a1... resolving heroku issues
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
